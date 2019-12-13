dd049403bc4f71c86871431f56e2b90e31a57018
mastekator <akmiskam@yandex.ru>
2019-12-13 16:09:36 +0300

    страницы с формой атворизации

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout";
 import Quiz from "./containers/Quiz/Quiz";
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import About from "./components/About/About";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizList from "./containers/QuizList/QuizList";
+import Auth from "./containers/Auth/Auth";
 
 function App() {
     return (
         <div className="App">
-            <Route path="/about" exact component={About}/>
             <Layout>
-                <Route path="/" exact component={Quiz}/>
+                <Switch>
+                    <Route path="/auth" exact component={Auth}/>
+                    <Route path="/quiz-creator" component={QuizCreator}/>
+                    <Route path="/quiz/:id" exact component={Quiz}/>
+                    <Route path="/" component={QuizList}/>
+                </Switch>
             </Layout>
         </div>
     );
--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './FinishedQuiz.module.css'
 import Button from "../UI/Button/Button";
+import {Link} from "react-router-dom";
 
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -36,7 +37,11 @@ const FinishedQuiz = props => {
 
             <div>
                 <Button onClick={props.onRetry} type="primary">Повторить</Button>
-                <Button onClick={props.onRetry} type="success">К списку тестов</Button>
+                <Link
+                to={'/'}
+                >
+                    <Button type="success">К списку тестов</Button>
+                </Link>
             </div>
         </div>
     )
