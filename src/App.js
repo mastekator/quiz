1347db3157b6027f028aee6f5cbce9bd8f811b46
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-01-26 21:04:45 +0300

    завершение редакса + хостинг

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Layout from "./hoc/Layout/Layout";
 import Quiz from "./containers/Quiz/Quiz";
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect, withRouter} from 'react-router-dom'
 import QuizCreator from "./containers/QuizCreator/QuizCreator";
 import QuizList from "./containers/QuizList/QuizList";
 import Auth from "./containers/Auth/Auth";
+import {connect} from "react-redux";
+import Logout from "./components/Logout/Logout";
+import {autoLogin} from "./store/actions/auth";
 
-function App() {
-    return (
-        <div className="App">
-            <Layout>
+class App extends Component {
+
+    componentDidMount() {
+        this.props.autoLogin()
+    }
+
+    render() {
+
+        let routes = (
+            <Switch>
+                <Route path="/auth" exact component={Auth}/>
+                <Route path="/quiz/:id" exact component={Quiz}/>
+                <Route path="/" exact component={QuizList}/>
+                <Redirect to={'/'}/>
+            </Switch>
+        )
+
+        if (this.props.isAuthenticated) {
+            routes = (
                 <Switch>
-                    <Route path="/auth" exact component={Auth}/>
                     <Route path="/quiz-creator" component={QuizCreator}/>
                     <Route path="/quiz/:id" exact component={Quiz}/>
-                    <Route path="/" component={QuizList}/>
+                    <Route path="/logout" component={Logout}/>
+                    <Route path="/" exact component={QuizList}/>
+                    <Redirect to={'/'}/>
                 </Switch>
-            </Layout>
-        </div>
-    );
+            )
+        }
+
+        return (
+            <div className="App">
+                <Layout>
+                    {routes}
+                </Layout>
+            </div>
+        );
+    }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.auth.token
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        autoLogin: () => dispatch(autoLogin())
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
